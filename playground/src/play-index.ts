adcd6e0998e05d5cc971ae23122c955b0e1ac971
안택수[Smith][동영상2개발] <smith.an@navercorp.com>
2019-12-26 20:14:26 +0900

    feat:playground test coded

--- playground/src/play-index.ts
+++ playground/src/play-index.ts
@@ -1,20 +1,15 @@
-class Foo {
-    private _name!: string;
-    constructor() {
+function (decorators, target, key, desc) {
+    var argumentLegnth = arguments.length, prototype_descriptor = argumentLegnth < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, currentDecorator;
+    
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function"){
+      prototype_descriptor = Reflect.decorate(decorators, target, key, desc)
+    };
+    else {
+      for (var i = decorators.length - 1; i >= 0; i--){
+        if (currentDecorator = decorators[i]) {
+          prototype_descriptor = (argumentLegnth < 3 ? currentDecorator(prototype_descriptor) : argumentLegnth > 3 ? currentDecorator(target, key, prototype_descriptor) : currentDecorator(target, key)) || prototype_descriptor
+        }
+      }
     }
-
-    public foo1(): number {
-        return 10;
-    }
-
-    public foo2(): string {
-        return this._name;
-    }
-
-    private _foo3(): void {
-        return;
-    }
-}
-
-const myFoo = new Foo();
-console.log(myFoo.foo1());
+    return argumentLegnth > 3 && prototype_descriptor && Object.defineProperty(target, key, prototype_descriptor), prototype_descriptor;
+};
--- playground/src/vue-index.ts
+++ playground/src/vue-index.ts
@@ -7,3 +7,19 @@ const naver: any = {
 naver.CorePlayer = CorePlayer;
 
 (window as Window & { naver: any }).naver = naver;
+
+const video = document.querySelector('video');
+video.volume = 0.1;
+document.addEventListener('dblclick', ()=>{
+    document.querySelector('video').volume = 1.0;
+});
+
+// video.attributes.crossorigin = "anonymous";
+
+// const audioContext = new AudioContext();
+// const videoNode = audioContext.createMediaElementSource(video);
+// const gainNode = audioContext.createGain();
+
+// videoNode.connect(gainNode);
+// gainNode.connect(audioContext.destination);
+// gainNode.gain.value = 5.0;
